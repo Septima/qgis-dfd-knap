e7d25e83c8418389238bfbe6fb7e22acf3d37d18
mie winstrup <mie@septima.dk>
2016-09-21 12:59:12 +0200

    Oversættelse fra engelsk til dansk

--- Datafordeler/i18n/da.ts
+++ Datafordeler/i18n/da.ts
@@ -1,82 +1,115 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="2.0">
 <context>
-    <name>Kortforsyningen</name>
+    <name>@default</name>
     <message>
-        <location filename="kortforsyningen.py" line="288"/>
-        <source>Kortforsyningen</source>
-        <translation>Kortforsyningen</translation>
+        <location filename="test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation>God morgen</translation>
+    </message>
+</context>
+<context>
+    <name>Datafordeler</name>
+    <message>
+        <location filename="plugin.py" line="151"/>
+        <source>No contact to Datafordeleren</source>
+        <translation>Ingen kontakt til Datafordeleren</translation>
     </message>
     <message>
-        <location filename="kortforsyningen.py" line="102"/>
-        <source>No contact to Kortforsyning</source>
-        <translation>Ingen kontakt til Kortforsyningen</translation>
-    </message>
-    <message>
-        <location filename="kortforsyningen.py" line="134"/>
-        <source>No contact to Kortforsyningen</source>
-        <translation>Ingen kontakt til Kortforsyningen</translation>
-    </message>
-    <message>
-        <location filename="kortforsyningen.py" line="324"/>
-        <source>Settings</source>
-        <translation>Indstillinger</translation>
-    </message>
-    <message>
-        <location filename="kortforsyningen.py" line="270"/>
+        <location filename="plugin.py" line="287"/>
         <source>Error</source>
         <translation>Fejl</translation>
     </message>
     <message>
-        <location filename="kortforsyningen.py" line="270"/>
-        <source>Could not load the layer. Is username and password correct?</source>
-        <translation>Kunne ikke indlæse laget. Er brugernavn og password korrekte?</translation>
+        <location filename="plugin.py" line="259"/>
+        <source>Please, fill out username and password</source>
+        <translation>Udfyld brugernavn og kodeord</translation>
     </message>
     <message>
-        <location filename="kortforsyningen.py" line="333"/>
+        <location filename="plugin.py" line="343"/>
+        <source>Settings</source>
+        <translation>Indstillinger</translation>
+    </message>
+    <message>
+        <location filename="plugin.py" line="287"/>
+        <source>Could not load the layer. Is username and password correct?</source>
+        <translation>Kunne ikke hente laget. Er brugernavn og kodeord korrekt?</translation>
+    </message>
+    <message>
+        <location filename="plugin.py" line="307"/>
+        <source>Datafordeler</source>
+        <translation>Datafordeler</translation>
+    </message>
+    <message>
+        <location filename="plugin.py" line="352"/>
         <source>About the plugin</source>
         <translation>Om pluginet</translation>
     </message>
     <message>
-        <location filename="kortforsyningen.py" line="242"/>
-        <source>Please, fill out username and password</source>
-        <translation>Udfyld venligst brugernavn og kodeord.</translation>
+        <location filename="plugin.py" line="364"/>
+        <source>Developed by Septima</source>
+        <translation>Udviklet af Septima</translation>
     </message>
     <message>
-        <location filename="kf_settings.ui" line="42"/>
-        <source>Username</source>
-        <translation>Brugernavn</translation>
+        <location filename="plugin.py" line="367"/>
+        <source>Developed by Septima</source>
+        <translation>Udviklet af Septima</translation>
+    </message>
+    <message>
+        <location filename="plugin.py" line="377"/>
+        <source>Powered by Datafordeler</source>
+        <translation>Powered af Datafordeler</translation>
     </message>
 </context>
 <context>
-  <name>s</name>
+    <name>Dialog</name>
     <message>
-        <location filename="kf_settings.ui" line="14"/>
-        <source>Access to Kortforsyningen.dk</source>
-        <translation>Adgang til Kortforsyningen.dk</translation>
+        <location filename="about.ui" line="22"/>
+        <source>About the plugin</source>
+        <translation>Om pluginet</translation>
     </message>
     <message>
-        <location filename="kf_settings.ui" line="42"/>
+        <location filename="aboutDatafordeler.ui" line="22"/>
+        <source>Datafordeler</source>
+        <translation>Datafordeler</translation>
+    </message>
+    <message>
+        <location filename="aboutSeptima.ui" line="22"/>
+        <source>Septima</source>
+        <translation>Septima</translation>
+    </message>
+</context>
+<context>
+    <name>s</name>
+    <message>
+        <location filename="settings.ui" line="14"/>
+        <source>Access to Datafordeler</source>
+        <translation>Adgang til Datafordeler</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="29"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Datafordeler&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Datafordeler&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="45"/>
         <source>Username</source>
         <translation>Brugernavn</translation>
     </message>
     <message>
-        <location filename="kf_settings.ui" line="55"/>
+        <location filename="settings.ui" line="55"/>
         <source>Password</source>
         <translation>Kodeord</translation>
     </message>
     <message>
-        <location filename="kf_settings.ui" line="68"/>
+        <location filename="settings.ui" line="84"/>
         <source>Remember settings</source>
         <translation>Gem indstillinger</translation>
     </message>
-</context>
-<context>
-  <name>Dialog</name>
     <message>
-        <location filename="aboutKortforsyningen.ui" line="22"/>
-        <source>About the plugin</source>
-        <translation>Om pluginet</translation>
+        <location filename="settings.ui" line="91"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;right&quot;&gt;&lt;a href=&quot;https://selfservice.datafordeler.dk/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Create datafordeler-user&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;right&quot;&gt;&lt;a href=&quot;https://selfservice.datafordeler.dk/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Create datafordeler-user&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 </TS>
