de8fb5a24e6dee2971b2dcab00ac27d82e406b1d
AsgerPetersen <asgerpetersen@gmail.com>
2016-11-04 11:23:28 +0100

    Updated translation file

--- Datafordeler/i18n/da.ts
+++ Datafordeler/i18n/da.ts
@@ -41,24 +41,14 @@
         <translation>Datafordeler</translation>
     </message>
     <message>
-        <location filename="plugin.py" line="352"/>
+        <location filename="plugin.py" line="353"/>
         <source>About the plugin</source>
         <translation>Om pluginet</translation>
     </message>
     <message>
-        <location filename="plugin.py" line="364"/>
+        <location filename="plugin.py" line="368"/>
         <source>Developed by Septima</source>
         <translation>Udviklet af Septima</translation>
-    </message>
-    <message>
-        <location filename="plugin.py" line="367"/>
-        <source>Developed by Septima</source>
-        <translation>Udviklet af Septima</translation>
-    </message>
-    <message>
-        <location filename="plugin.py" line="377"/>
-        <source>Powered by Datafordeler</source>
-        <translation>Powered af Datafordeler</translation>
     </message>
 </context>
 <context>
@@ -87,29 +77,33 @@
         <translation>Adgang til Datafordeler</translation>
     </message>
     <message>
-        <location filename="settings.ui" line="29"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Datafordeler&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Datafordeler&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
-    </message>
-    <message>
-        <location filename="settings.ui" line="45"/>
+        <location filename="settings.ui" line="62"/>
         <source>Username</source>
         <translation>Brugernavn</translation>
     </message>
     <message>
-        <location filename="settings.ui" line="55"/>
+        <location filename="settings.ui" line="72"/>
         <source>Password</source>
         <translation>Kodeord</translation>
     </message>
     <message>
-        <location filename="settings.ui" line="84"/>
+        <location filename="settings.ui" line="92"/>
         <source>Remember settings</source>
         <translation>Gem indstillinger</translation>
     </message>
     <message>
-        <location filename="settings.ui" line="91"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;right&quot;&gt;&lt;a href=&quot;https://selfservice.datafordeler.dk/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Create datafordeler-user&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;right&quot;&gt;&lt;a href=&quot;https://selfservice.datafordeler.dk/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Create datafordeler-user&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <location filename="settings.ui" line="29"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Datafordeler service user&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settings.ui" line="36"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Access to the data from Datafordeler requires a service-user login (in Danish: &apos;Tjenestebruger&apos;). &lt;a href=&quot;http://datafordeler.dk/vejledninger/brugeroprettelse/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Read more about creating a service user&lt;/span&gt;&lt;/a&gt; and &lt;a href=&quot;https://selfservice.datafordeler.dk/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;create a user&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
